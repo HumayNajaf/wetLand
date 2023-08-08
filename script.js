8b98d7182bc16338d9f9be98f43763139799c2e1
HumayNajafova <humaynecefovs@gmail.com>
2023-08-08 10:33:10 +0400

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const openBtn = document.querySelector('.open-btn')
+const openBtn = document.querySelector(".open-btn");
 const btn = document.querySelector(".btn");
 const menu = document.querySelector("nav");
+const xIcon = document.querySelectorAll(".x-icon");
+const ddContainer = document.querySelectorAll(".home-drop-down");
 
-openBtn.addEventListener('click', () => {
-    menu.classList.add('show')
-})
+openBtn.addEventListener("click", () => {
+  menu.classList.add("show");
+});
 btn.addEventListener("click", () => {
   menu.classList.remove("show");
-
 });
