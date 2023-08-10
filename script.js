5bd96bf33d6faace9f3c3fbc1521b30a51078fe1
HumayNajafova <humaynecefovs@gmail.com>
2023-08-10 08:54:42 +0400

    update

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const openBtn = document.querySelector(".open-btn");
 const btn = document.querySelector(".btn");
 const menu = document.querySelector("nav");
 const xIcon = document.querySelectorAll(".x-icon");
-const ddContainer = document.querySelectorAll(".home-drop-down");
+const menuList = document.querySelectorAll('.home-drop-down')
+const dropDownMenu = document.querySelectorAll('.drop-down-menu')
 
 openBtn.addEventListener("click", () => {
   menu.classList.add("show");
@@ -10,3 +11,20 @@ openBtn.addEventListener("click", () => {
 btn.addEventListener("click", () => {
   menu.classList.remove("show");
 });
+
+menuList.forEach(el=>{
+el.addEventListener('click',()=>{
+ if(!el.classList.contains('open-menu')){
+  removeAll()
+  el.classList.add('open-menu')
+ }else{
+  el.classList.remove('open-menu')
+ }
+})  
+})
+
+function removeAll(){
+  menuList.forEach(menuEl=>{
+    menuEl.classList.remove('open-menu')
+  })
+}
